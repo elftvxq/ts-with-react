20da70e74ba9ea3c8af0a05a75acd867b259df65
Emma Shih <emmashih721@hotmail.com>
2020-11-12 00:39:10 +0800

    customized hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Hello from './components/Hello'
+import useURLLoader from './hooks/useURLLoader'
+interface IShowResult {
+	message: string;
+	status: string;
+}
+interface IThemeProps {
+	[key: string]: { color: string; background: string; }
+}
 
-function App() {
+const App: React.FC = () => {
+	const [show, setShow] = useState(true)
+	const [data, loading] = useURLLoader('https://dog.ceo/api/breeds/image/random', [show])
+	const dogResult = data as IShowResult
+
 	return (
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
 				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+					<button onClick={() => { setShow(!show) }}>Refresh Dog</button>
+				</p>
+				{loading ? <p>讀取中</p>
+					: <img src={dogResult && dogResult.message} />
+				}
 				<Hello message="Hello World" />
 				<a
 					className="App-link"
